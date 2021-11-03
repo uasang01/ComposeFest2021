5b7dc6c5c004727b1470c198538196c3505e138f
김상훈 <uasang01@name.com>
2021-11-04 00:38:08 +0900

    Complete up to Chapter 12

--- week 1-Jetpack Compose basics/BasicsCodelab_Sean/app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicsCodelab_Sean/app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.basicscodelab
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basicscodelab.ui.theme.BasicsCodelabTheme
@@ -25,7 +34,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun MyApp() {
-    var shouldShowOnBoarding by remember { mutableStateOf(true) }
+    var shouldShowOnBoarding by rememberSaveable { mutableStateOf(true) }
 
     if (shouldShowOnBoarding) {
         OnboardingScreen(onContinueClicked = { shouldShowOnBoarding = false })
@@ -35,12 +44,17 @@ private fun MyApp() {
 }
 
 @Composable
-private fun Greetings(names: List<String> = listOf("World", "Compose")) {
-    Column(modifier = Modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
-            Greeting(name)
+private fun Greetings(names: List<String> = List(1000) { "$it" }) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
+            Greeting(name = name)
         }
     }
+//    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+//        for (name in names) {
+//            Greeting(name)
+//        }
+//    }
 }
 
 
@@ -67,8 +81,16 @@ fun OnboardingScreen(onContinueClicked: () -> Unit) {
 
 @Composable
 fun Greeting(name: String) {
-    val expended = remember { mutableStateOf(false) }
-    val extraPadding = if (expended.value) 48.dp else 0.dp
+    var expended by rememberSaveable { mutableStateOf(false) }
+
+    val extraPadding by animateDpAsState(
+        targetValue = if (expended) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow,
+        ),
+        finishedListener = { Log.d("Greeting", "animation finished") }
+    )
 
     Surface(
         color = MaterialTheme.colors.primary,
@@ -78,26 +100,34 @@ fun Greeting(name: String) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello, ")
-                Text(text = name)
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.h4.copy(fontWeight = FontWeight.ExtraBold)
+                )
             }
             OutlinedButton(
-                onClick = { expended.value = !expended.value }
+                onClick = { expended = !expended }
             ) {
-                Text(if (expended.value) "Show less" else "Show more")
+                Text(if (expended) "Show less" else "Show more")
             }
         }
     }
 }
 
-
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     BasicsCodelabTheme {
-        MyApp()
+        Greetings()
     }
 }
 
