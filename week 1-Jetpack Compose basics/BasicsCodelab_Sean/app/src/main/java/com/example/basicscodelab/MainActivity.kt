ed92d68f0f6f4cde6a307f3ece83dee267cb1623
김상훈 <uasang01@name.com>
2021-11-04 23:48:57 +0900

    Complete up Jetpack Compose basics

--- week 1-Jetpack Compose basics/BasicsCodelab_Sean/app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicsCodelab_Sean/app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.basicscodelab
 
+import android.content.Context
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.content.res.Resources
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
@@ -12,10 +15,14 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,21 +51,6 @@ private fun MyApp() {
 }
 
 @Composable
-private fun Greetings(names: List<String> = List(1000) { "$it" }) {
-    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
-        items(items = names) { name ->
-            Greeting(name = name)
-        }
-    }
-//    Column(modifier = Modifier.padding(vertical = 4.dp)) {
-//        for (name in names) {
-//            Greeting(name)
-//        }
-//    }
-}
-
-
-@Composable
 fun OnboardingScreen(onContinueClicked: () -> Unit) {
     // TODO: This state should be hoisted
 
@@ -80,39 +72,69 @@ fun OnboardingScreen(onContinueClicked: () -> Unit) {
 }
 
 @Composable
+private fun Greetings(names: List<String> = List(1000) { "$it" }) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
+            Greeting(name = name)
+        }
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
-    var expended by rememberSaveable { mutableStateOf(false) }
-
-    val extraPadding by animateDpAsState(
-        targetValue = if (expended) 48.dp else 0.dp,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow,
-        ),
-        finishedListener = { Log.d("Greeting", "animation finished") }
-    )
-
-    Surface(
-        color = MaterialTheme.colors.primary,
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
-            ) {
-                Text(text = "Hello, ")
+        CardContent(name)
+    }
+}
+
+@Composable
+private fun CardContent(name: String) {
+    var expended by rememberSaveable { mutableStateOf(false) }
+
+
+    Row(
+        modifier = Modifier
+            .padding(24.dp)
+            .animateContentSize(
+                animationSpec =
+                spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello, ")
+            Text(
+                text = name,
+                style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            if (expended) {
                 Text(
-                    text = name,
-                    style = MaterialTheme.typography.h4.copy(fontWeight = FontWeight.ExtraBold)
+                    text = ("Composem ipsum color sit lazy, " +
+                            "padding theme elit, sed do bouncy. ").repeat(4),
+                    style = MaterialTheme.typography.subtitle1
                 )
             }
-            OutlinedButton(
-                onClick = { expended = !expended }
-            ) {
-                Text(if (expended) "Show less" else "Show more")
-            }
+        }
+        IconButton(onClick = { expended = !expended }, Modifier.padding(0.dp)) {
+            Icon(
+                imageVector =
+                if (expended) Icons.Filled.ExpandLess
+                else Icons.Filled.ExpandMore,
+                contentDescription =
+                if (expended) stringResource(id = R.string.show_less)
+                else stringResource(id = R.string.show_more)
+            )
         }
     }
 }
