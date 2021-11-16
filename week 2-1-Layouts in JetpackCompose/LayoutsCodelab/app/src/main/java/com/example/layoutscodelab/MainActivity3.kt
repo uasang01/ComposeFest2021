6b15d2d5c45b0fc18de352208805a64ca7bae0df
김상훈 <uasang01@name.com>
2021-11-16 23:38:50 +0900

    Completed up to Chapter 7 in LayoutsCodelab

--- week 2-1-Layouts in JetpackCompose/LayoutsCodelab/app/src/main/java/com/example/layoutscodelab/MainActivity3.kt
+++ week 2-1-Layouts in JetpackCompose/LayoutsCodelab/app/src/main/java/com/example/layoutscodelab/MainActivity3.kt
@@ -18,13 +18,18 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.example.layoutscodelab.ui.theme.LayoutsCodelabTheme
 import kotlinx.coroutines.launch
 
-class MainActivity2 : AppCompatActivity() {
+class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -32,6 +37,89 @@ class MainActivity2 : AppCompatActivity() {
                 LayoutsCodelab()
             }
         }
+    }
+}
+
+fun Modifier.firstBaselineToTop(
+    firstBaselineToTop: Dp
+) = this.then(
+        layout { measurable, constraints ->
+            val placeable = measurable.measure(constraints = constraints)
+
+            // Check the composable has a first baseline
+            check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+            val firstBaseline = placeable[FirstBaseline]
+
+            // Height of the composable with padding - first baseline
+            val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+            val height = placeable.height + placeableY
+            layout(placeable.width, height) {
+                // Where the composable gets placed
+                placeable.placeRelative(0, placeableY)
+            }
+        }
+    )
+
+
+@Preview
+@Composable
+fun TextWithPaddingToBaselinePreview() {
+    LayoutsCodelabTheme {
+        Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
+    }
+}
+
+@Preview
+@Composable
+fun TextWithNormalPaddingPreview() {
+    LayoutsCodelabTheme {
+        Text("Hi there!", Modifier.padding(top = 32.dp))
+    }
+}
+
+@Composable
+fun MyOwnColumn(
+    modifier: Modifier = Modifier,
+    // custom layout attributes
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        // measure and position children given constraints logic here
+
+        // Don't constrain child views further, measure them with given constraints
+        // List of measured children
+        val placeables = measurables.map { measurable ->
+            // Measure each child
+            measurable.measure(constraints)
+        }
+
+        // Track the y co-ord we have placed children up to
+        var yPosition = 0
+
+        // Set the size of the layout as big as it can
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            // Place children in the parent layout
+            placeables.forEach { placeable ->
+                // Position item on the screen
+                placeable.placeRelative(x = 0, y = yPosition)
+
+                // Record the y co-ord placed up to
+                yPosition += placeable.height
+            }
+        }
+    }
+}
+
+@Composable
+private fun BodyContent(modifier: Modifier = Modifier) {
+    MyOwnColumn(modifier.padding(8.dp)) {
+        Text("MyOwnColumn")
+        Text("places items")
+        Text("vertically.")
+        Text("We've done it by hand!")
     }
 }
 
@@ -51,76 +139,7 @@ private fun LayoutsCodelab() {
             )
         }
     ) { innerPadding ->
-//        BodyContent(Modifier.padding(innerPadding))
-        ScrollingList(Modifier.padding(innerPadding))
-    }
-}
-
-@Composable
-fun ScrollingList(modifier: Modifier) {
-
-    // We save the scrolling position with this state that can also
-    // be used to programmatically scroll the list
-    val scrollState = rememberLazyListState()
-
-    val listSize = 100
-
-    // We save the coroutine scope where our animated scroll will be executed
-    val coroutineScope = rememberCoroutineScope()
-
-    Column(
-        modifier = modifier
-    ) {
-        Row{
-            Button(onClick = {
-                coroutineScope.launch {
-                    // 0 is the first item index
-                    scrollState.animateScrollToItem(0)
-                }
-            }) {
-                Text(text = "Scroll to the top")
-            }
-            Button(onClick = {
-                coroutineScope.launch {
-                    // listSize - 1 is the last index of the list
-                    scrollState.animateScrollToItem(listSize-1)
-                }
-            }) {
-                Text(text = "Scroll to the bottom")
-            }
-        }
-
-        LazyColumn(state = scrollState) {
-            items(100){
-                ImageListItem(it)
-            }
-        }
-    }
-
-}
-
-@Composable
-fun ImageListItem(index: Int){
-    Row(verticalAlignment = Alignment.CenterVertically){
-        Image(
-            painter = rememberImagePainter(
-                data = "https://developer.android.com/images/brand/Android_Robot.png"
-            ),
-            contentDescription = "Android Logo",
-            modifier = Modifier.size(50.dp)
-        )
-        Spacer(Modifier.width(10.dp))
-        Text("Item #$index", style = MaterialTheme.typography.subtitle1)
-    }
-}
-
-@Composable
-fun BodyContent(modifier: Modifier) {
-    Column(
-        modifier = modifier.padding(8.dp)
-    ) {
-        Text(text = "Hi there!")
-        Text(text = "Thanks for going through the Layouts codelab")
+        BodyContent(Modifier.padding(8.dp))
     }
 }
 
